84f29f9cfedfdd15baa48dad5019f612bdc77f2d
Erik Onarheim <erik.onarheim@gmail.com>
2026-02-13 12:07:51 -0600

    chore: update to latest ex 0.32.0

--- src/ground.ts
+++ src/ground.ts
@@ -3,35 +3,46 @@ import { Resources } from "./resources";
 import { Config } from "./config";
 
 export class Ground extends ex.Actor {
-    groundSprite = Resources.GroundImage.toSprite();
-    moving = false;
-    constructor(pos: ex.Vector) {
-        super({
-            pos,
-            anchor: ex.vec(0, 0),
-            height: 64,
-            width: 400,
-            z: 1
-        })
-    }
+  groundSprite = Resources.GroundImage.toSprite();
+  moving = false;
+  constructor(pos: ex.Vector) {
+    super({
+      pos,
+      anchor: ex.vec(0, 0),
+      height: 64,
+      width: 400,
+      z: 1
+    })
+  }
 
-    onInitialize(engine: ex.Engine): void {
-        this.groundSprite.sourceView.width = engine.screen.drawWidth;
-        this.groundSprite.destSize.width = engine.screen.drawWidth;
-        this.graphics.use(this.groundSprite);
-    }
+  onInitialize(engine: ex.Engine): void {
 
-    onPostUpdate(_engine: ex.Engine, elapsedMs: number): void {
-        if (!this.moving) return;
-        this.groundSprite.sourceView.x += Config.PipeSpeed * (elapsedMs / 1000);
-        this.groundSprite.sourceView.x = this.groundSprite.sourceView.x % Resources.GroundImage.width;
-    }
+    this.groundSprite = new ex.TiledSprite({
+      image: Resources.GroundImage,
+      width: engine.screen.drawWidth,
+      height: 64,
+      wrapping: {
+        x: ex.ImageWrapping.Repeat,
+        y: ex.ImageWrapping.Clamp
+      }
+    });
 
-    start() {
-        this.moving = true;
-    }
+    this.graphics.use(this.groundSprite);
+  }
 
-    stop() {
-        this.moving = false;
-    }
+  onPostUpdate(_engine: ex.Engine, elapsedMs: number): void {
+    if (!this.moving) return;
+    // scroll the tiled sprite to the right, clamp to the width to keep the uv's small preventing visual
+    // artifacts
+    this.groundSprite.sourceView.x += Config.PipeSpeed * (elapsedMs / 1000);
+    this.groundSprite.sourceView.x = this.groundSprite.sourceView.x % Resources.GroundImage.width;
+  }
+
+  start() {
+    this.moving = true;
+  }
+
+  stop() {
+    this.moving = false;
+  }
 }
